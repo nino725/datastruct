5e9803218d72a70860243c1a8c575a13c367f39c
wjx13753607566@163.com <227969083+nino725@users.noreply.github.com>
2026-02-07 17:59:39 +0800

    LinearList

--- LinearList/SqList.cpp
+++ LinearList/SqList.cpp
@@ -90,3 +90,77 @@ bool reverseNum(SqList& L){
     }
     return true;
 }
+
+//三
+bool deleteX(SqList& L,ElemType x){
+    //n为长度
+    int n = L.length;
+    if(n <= 0){
+        return false;
+    }
+    int j = 0;
+    for(int i = 0; i < n; i++){
+        if(L.data[i] != x){
+            L.data[j++] = L.data[i];
+        }
+    }
+    n = j;
+    return true;
+}
+
+//四
+bool deleteSToT(SqList& L, ElemType s, ElemType t){
+    if(L.length == 0){
+        return false;
+    }
+    if(s >= t){
+        return false;
+    }
+    for(int i = 0; i < L.length; i++){
+        if(L.data[i] >= s && L.data[i] <= t){
+            for(int j = i + 1; j < L.length; j++){
+                L.data[j-1] = L.data[j];
+                L.length--;
+            }
+        }
+    }
+    return true;
+}
+
+//五。
+bool deleteSame(SqList& L){
+    if(L.length == 0){
+        return false;
+    }
+    for(int i = 1; i < L.length; i++){
+        if(L.data[i] == L.data[i-1]){
+            for(int j = i + 1; j < L.length; j++){
+                L.data[j-1] = L.data[j];
+                L.length--;
+            }
+        }
+    }
+    return true;
+}
+
+//6
+bool merge(SqList P, SqList Q,SqList& S){
+    if(P.length + Q.length > S.MAXSIZE){
+        return false;
+    }
+    int i = 0, j = 0;
+    while(i < P.length && j < Q.length){
+        if(P.data[i] <= Q.data[i]){
+            S.data[S.length++] = P.data[i]; 
+        }else{
+            S.data[S.length++] = Q.data[i];
+        }
+    }
+    while(i != P.length){
+        S.data[S.length++] = P.data[i++];
+    }
+    while(j != Q.length){
+        S.data[S.length++] = Q.data[j++];
+    }
+    return true;
+}
